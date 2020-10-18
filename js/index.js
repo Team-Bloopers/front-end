7dbd1a42b2c80901d1d682e9cd76016b7e87fe68
vanshamb2601 <59662860+vanshamb2601@users.noreply.github.com>
2020-10-18 22:11:36 +0530

    profile and contact page added

--- js/index.js
+++ js/index.js
@@ -21,3 +21,15 @@ window.onscroll = function(){
   let progressHeight = (window.pageYOffset / totalHeight) *100;
   progress.style.height = progressHeight + "%";
 }
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/codepen/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
